9f8fcb42ab6cddc7ed6c136813bd404c3ad098b6
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-08-31 17:31:13 -0500

    feat: add validations in dto

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,33 @@
+import {
+  IsIn,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 import { TaskStatus } from '../task.entity';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   title: string;
+
+  @IsString()
   description: string;
 }
 
 export class UpdateTaskDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status?: TaskStatus;
 }
