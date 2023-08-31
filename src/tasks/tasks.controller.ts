2695dcf6598d25028c34e6551b73394b2973ada6
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2023-08-31 16:42:54 -0500

    feat: create and list tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/task.dto';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
   @Get()
-  helloworld() {
-    return 'Hello World';
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDto) {
+    return this.tasksService.createTasks(newTask.title, newTask.description);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+import { v4 } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  // simulate a database
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'first task',
+      description: 'some task',
+      status: TaskStatus.PENDING,
+    },
+  ];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+
+  createTasks(title: string, description: string) {
+    const task = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    };
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  // updateTasks() {}
+
+  // deleteTasks() {}
+}
